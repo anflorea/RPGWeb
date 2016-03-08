695cf43180f703e072e31f85871f70f106fbf61c
anflorea <floreaandreimihai@gmail.com>
2016-03-08 11:39:58 +0200

    Login now works (still have to fix that message when login fails)

---
+++ src/main/java/ro/academyplus/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package ro.academyplus.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Ghebo on 1/6/2016.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DatabaseAuthenticationProvider authenticationProvider;
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring().antMatchers("/js/**", "/css/**", "/img/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/login").failureUrl("/?failed=true").defaultSuccessUrl("/")
+                .and().logout().logoutSuccessUrl("/").and().authorizeRequests()
+                .antMatchers("/", "/login", "/register").permitAll().anyRequest()
+                .authenticated().and().csrf().disable();
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.authenticationProvider(authenticationProvider).eraseCredentials(true);
+    }
+}
